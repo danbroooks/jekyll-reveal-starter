7fa9b575862d622d54e46ca39a58ab4ce7b85a64
Dan Brooks <dmbrooks@live.co.uk>
2017-03-10 00:06:36 +0000

    Get webpack pulling styles through

--- src/script.js
+++ src/script.js
@@ -1,3 +1,16 @@
-require('./styles.scss');
 
-console.log("hello world!");
+window.Reveal = require('reveal.js');
+
+Reveal.initialize({
+  history: true,
+  transition: 'slide',
+  transitionSpeed: 'fast',
+});
+
+require('reveal.js/plugin/markdown/marked.js');
+require('reveal.js/plugin/markdown/markdown.js');
+require('reveal.js/plugin/notes/notes.js');
+
+window.hljs = require('highlight.js');
+require('./highlight-reveal.js');
+hljs.initHighlightingOnLoad();
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,62 @@
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-  entry: './src/script.js',
+  entry: [
+    './src/script.js',
+    './src/styles.scss'
+  ],
   output: {
     path: 'presentation',
     filename: 'script.js'
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.scss$/,
         loader: ExtractTextPlugin.extract({
           fallback: 'style-loader',
           use: [
             'css-loader',
-            'sass-loader'
+            'sass-loader',
           ]
         }),
-      }
+      },
+      {
+        test: /\.eot(\?\S*)?$/,
+        loader: 'url-loader?limit=100000&mimetype=application/vnd.ms-fontobject'
+      },
+      {
+        test: /\.woff2(\?\S*)?$/,
+        loader: 'url-loader?limit=100000&mimetype=application/font-woff2'
+      },
+      {
+        test: /\.woff(\?\S*)?$/,
+        loader: 'url-loader?limit=100000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.ttf(\?\S*)?$/,
+        loader: 'url-loader?limit=100000&mimetype=application/font-ttf'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file-loader"
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url-loader?limit=10000&mimetype=image/svg+xml"
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.jpg$/,
+        loader: "file-loader"
+      },
+      {
+        test: /\.png$/,
+        loader: "url-loader?mimetype=image/png"
+      },
     ]
   },
   plugins: [
